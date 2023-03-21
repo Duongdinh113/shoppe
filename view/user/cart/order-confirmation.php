3c30bae26f3b8e192ffd1e1dd19db38b4cf253e0
thanh02 <122164005+thanhle02@users.noreply.github.com>
2023-03-21 13:00:15 +0700

    Add files via upload

---
+++ view/user/cart/order-confirmation.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ACCOUNT-ORDER</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="menu1">
+
+                <h1> SHOPPE </h1>
+            </div>
+            <div class="menu2">
+                <nav>
+                    <ul>
+                        <li><a href="">Shop </a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Our Story</a></li>
+                        <li><a href="">|</a></li>
+                        <li><a href=""><img src="../img/Icon color(2).png" alt=""></a></li>
+                        <li><a href=""> <img src="../img/Vector(1).png" alt=""></a></li>
+                        <li><a href=""><i class="fa-regular fa-user"></i></a></li>
+
+
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+        <hr>
+        <div class="content2">
+
+            <div class="menu3">
+                <ul>
+                    <li class="Dashboard"><a href="">Dashboard</a></li>
+                    <li><a href="">Orders</a></li>
+                    <li><a href="">Downloads</a></li>
+                    <li><a href="">Addresses</a></li>
+                    <li><a href="">Account details</a></li>
+                    <li><a href="">Logout</a></li>
+                </ul>
+                <div class="line">
+                    <div class="hr3">
+                        <hr>
+                    </div>
+                    <div class="hr1"><a href=""> <span>
+                                <hr>
+                            </span></a></div>
+                    <div class="hr2">
+                        <hr>
+                    </div>
+                </div>
+            </div>
+            <div class="hightline">
+                <hr>
+                <div class="text5">
+                    <div>
+                        <p>No downloads available yet.</p>
+                    </div>
+                    <div>
+                        <h3 style=" color: #A18A68; margin-right: 10px;">BROWSE PRODUCT</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="adress">
+                <div>
+                    <h3>Order Details</h3>
+                    <div class="adress2">
+                        <div class="order-number">
+                            <div class="adress-box">
+                                <h3>ORDER NUMBER</h3>
+                                <p>1879605573994</p>
+                            </div>
+                            <div class="adress-box">
+                                <h3>EMAIL</h3>
+                                <p>Vitathemes@gmail.com</p>
+                            </div>
+                            <div class="adress-box">
+                                <h3>PAYMENT METHOD</h3>
+                                <p>Mastercard*************7865</p>
+                            </div>
+                            <div class="adress-box">
+                                <h3>ORDER DATE</h3>
+                                <p>October 8,2020</p>
+                            </div>
+                        </div>
+
+
+                        <div class="delivery-option">
+
+                            <div class="adress-box">
+                                <h3>DELIVERY OPTIONS</h3>
+                                <p>Standard delivery</p>
+                            </div>
+                            <div class="adress-box">
+                                <h3>DELIVERY ADDRESS</h3>
+                                <p>Kristian holst 34 old street W1F 7NU london United Kingdom
+                                </p>
+                            </div>
+                            <div class="adress-box">
+
+                            </div>
+                            <div class="adress-box">
+                                <h3>CONTACT NUMBER</h3>
+                                <p>+44 8749790988</p>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="ORDER Summary">
+                    <h3>ORDER Summary</h3>
+                    <div class="total">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>PRODUCT</th>
+                                    <th>TOTAL</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Lira Earrings </td>
+                                    <td>$64</td>
+
+                                </tr>
+                                <tr>
+                                    <td>Ollie Earrings</td>
+                                    <td>$10</td>
+
+                                </tr>
+                                <tr>
+                                    <td>Kaede Hair Pin</td>
+                                    <td>$10</td>
+
+                                </tr>
+                                <tr>
+                                    <th>SUBTOTAL</th>
+                                    <th>$85</th>
+                                </tr>
+                                <tr>
+                                    <th>SHIPPING</th>
+                                    <td>FREE SHIPPING</td>
+                                </tr>
+                                <tr>
+                                    <th> TOTAL</th>
+                                    <td>$85</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="footer">
+            <div class="footer1">
+                <ul>
+                    <li><a href="">CONTACT</a></li>
+                    <li><a href="">TERMS OF SERVICES</a></li>
+                    <li><a href="">SHIPPING AND RETURNS</a></li>
+                </ul>
+                <div class="text2">
+                    <p>
+                    <h4> &#169; 2021 Shelly.</h4>
+                    <h5>Terms of use <strong>and</strong> privacy policy. </h5>
+                    </p>
+                </div>
+            </div>
+            <div class="footer2">
+                <div class="text3">
+                    <h4>Give an email, get the newsletter.</h4>
+                    <p> &#8594;</p>
+                </div>
+                <hr>
+                <ul>
+                    <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
+                    <li><a href=""> <i class="fa-brands fa-instagram"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-twitter"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-linkedin-in"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
