884f3c9c1119d4960bc0cb43e52372028731b4a9
thanh02 <122164005+thanhle02@users.noreply.github.com>
2023-03-21 10:20:52 +0700

    Add files via upload

---
+++ customer/account/account-dowload.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ACCOUNT-DOWLOAD</title>
+    <link rel="stylesheet" href="../css/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="menu1">
+
+                <h1> SHOPPE </h1>
+            </div>
+            <div class="menu2">
+                <nav>
+                    <ul>
+                        <li><a href="">Shop </a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Our Story</a></li>
+                        <li><a href="">|</a></li>
+                        <li><a href=""><img src="img/Icon color(2).png" alt=""></i></i></a></li>
+                        <li><a href=""> <img src="img/Vector(1).png" alt=""></a></li>
+                        <li><a href=""><i class="fa-regular fa-user"></i></a></li>
+
+
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+        <hr>
+        <div class="content2">
+
+            <div class="menu3">
+                <ul>
+                    <li class="Dashboard"><a href="">Dashboard</a></li>
+                    <li><a href="">Orders</a></li>
+                    <li><a href="">Downloads</a></li>
+                    <li><a href="">Addresses</a></li>
+                    <li><a href="">Account details</a></li>
+                    <li><a href="">Logout</a></li>
+                </ul>
+                <div class="line">
+                    <div class="hr3">
+                        <hr>
+                    </div>
+                    <div class="hr1"><a href=""> <span>
+                                <hr>
+                            </span></a></div>
+                    <div class="hr2">
+                        <hr>
+                    </div>
+                </div>
+            </div>
+            <div class="hightline">
+                <hr>
+                <div class="text5">
+                    <div>
+                        <p>No downloads available yet.</p>
+                    </div>
+                    <div>
+                        <h4 style=" color: #A18A68;
+                        margin-right: 10px;">BROWSE PRODUCT</h4>
+                    </div>
+                </div>
+            </div>
+            <table class="table orders">
+                <thead>
+                    <tr>
+                        <th scope="col">ORDER NUMBER</th>
+                        <th scope="col">DATE</th>
+                        <th scope="col">STATUS</th>
+                        <th scope="col">TOTAL</th>
+                        <th scope="col">ACTIONS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">7643980998990</td>
+                        <td>October 8,2021</td>
+                        <td>Delivered</td>
+                        <td>$ 105</td>
+                        <td class="action">View Order</td>
+
+                    </tr>
+                    <tr>
+                        <td scope="row">943980998990</td>
+                        <td>October 8,2021</td>
+                        <td>Processing</td>
+                        <td>$100</td>
+                        <td class="action">View Order</td>
+
+                    </tr>
+                    <tr>
+                        <td scope="row">879980998990</td>
+                        <td>October 8,2020</td>
+                        <td>Delivered</td>
+                        <td>$ 65</td>
+                        <td class="action">View Order</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+        <hr>
+        <div class="footer">
+            <div class="footer1">
+                <ul>
+                    <li><a href="">CONTACT</a></li>
+                    <li><a href="">TERMS OF SERVICES</a></li>
+                    <li><a href="">SHIPPING AND RETURNS</a></li>
+                </ul>
+                <div class="text2">
+                    <p>
+                    <h4> &#169; 2021 Shelly.</h4>
+                    <h5>Terms of use <strong>and</strong> privacy policy. </h5>
+                    </p>
+                </div>
+            </div>
+            <div class="footer2">
+                <div class="text3">
+                    <h4>Give an email, get the newsletter.</h4>
+                    <p> &#8594;</p>
+                </div>
+                <hr>
+                <ul>
+                    <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
+                    <li><a href=""> <i class="fa-brands fa-instagram"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-twitter"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-linkedin-in"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
