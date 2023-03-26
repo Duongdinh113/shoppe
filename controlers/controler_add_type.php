fe467cda3f7af39ebeab222f7d30c4bfb1f643af
Duongdinh113 <122246359+Duongdinh113@users.noreply.github.com>
2023-03-26 09:28:11 +0700

    Delete controlers directory

--- controlers/controler_add_type.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    require "../model/connect.php";
-    $type = $_POST["type"];
-    $query = "INSERT INTO type (name) VALUES ('$type')";
-    connect($query);
-    header("location:../view/admin/type.php");
-  
-?>
--- controlers/controler_delete-product.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require_once "../model/connect.php";
-$id = $_GET["id"];
-$query = "DELETE FROM `products` WHERE id_p = '$id'";
-connect($query);
-header("location:../view/admin/product.php");
-?>
