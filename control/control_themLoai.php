f89d29d837f64f1fd0606fec3f875d57d871db57
Truongtraj <117931383+Truongtraj@users.noreply.github.com>
2023-03-29 22:45:53 +0700

    Delete control directory

--- control/control_themLoai.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    require '../models/connect.php';
-    $Name = $_POST["loai"];
-
-    $query = "INSERT INTO categoryid (name) VALUES ('$Name')";
-    connect($query); //gọi hàm connect() để thực thi câu lệnh insert
-    header("location:../view/admin/quanLi_danhMuc.php");
-?>
