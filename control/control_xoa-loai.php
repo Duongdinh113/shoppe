ff7c90b51deba6868d0ffbae1e5b91e53647676b
thanh02 <122164005+thanhle02@users.noreply.github.com>
2023-03-30 09:53:48 +0700

    Delete control directory

--- control/control_xoa-loai.php
+++
@@ -1,8 +0,0 @@
-<?php
-// echo 1;die;
-    require "../models/connect.php";
-    $id = $_GET["id"];
-    $query = "DELETE FROM categoryid WHERE id = $id";
-    connect($query);
-    header("location:../view/admin/quanLi_danhMuc.php");
-?>
