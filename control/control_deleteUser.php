1541e6fb75ac1e20fd4ea481f5632f3438754567
Duongdinh113 <122246359+Duongdinh113@users.noreply.github.com>
2023-04-08 09:50:53 +0700

    Delete control directory

--- control/control_deleteUser.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require "../models/connect.php";
-    $id = $_GET["id"];
-    $query = "DELETE FROM users WHERE id = $id";
-    connect($query);
-    header("location:../view/admin/quanLi_user.php")
-?>
