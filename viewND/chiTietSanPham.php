336be9af78902076c6db1c8caf98dd28567b1a4b
Truongtraj <117931383+Truongtraj@users.noreply.github.com>
2023-04-10 23:57:35 +0700

    truongupdate

--- viewND/chiTietSanPham.php
+++ viewND/chiTietSanPham.php
@@ -8,37 +8,11 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./css/layout.css">
     <title>CHI TIẾT SẢN PHẨM </title>
 </head>
-<style>
-    .spct{
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-    }
-    .boxa img{
-        height: 150px;
-        width: 150px;
-        border: 1px solid #cdcdcd;
-        margin-top: 10%;
-    }
-    h1{
-        text-align: center;
-    }
-    .boxct img{
-        width: 500px;
-        height: 450px;
-    }
-    h3{
-        text-align: center;
-    }
-    .boxbl{
-        margin-left: 40%;
-        margin-top: 10%;
-    }
 
-</style>
 <body>
 
 
@@ -46,15 +20,14 @@ session_start();
     <div class="container">
         <header>
             <div class="logo">
-                <img src="image/SHOPPE.png" alt="">
+                <img src="image/logo_1.1.png" alt="">
             </div>
             <div class="menu">
                 <ul class="">
                     <li><a href="index.php">Trang Chủ</a></li>
                     <li><a href="sanpham.php">Sản Phẩm </a></li>
                     <li><a href="giam_gia.php">Hàng Giảm Giá </a></li>
-                    <li><a href="lienhe.php">Liên Hệ </a></li>
-                    <li><a href="donHangdaMua.php">Đơn Hàng </a></li>
+                    <li><a href="">Liên Hệ </a></li>
                     <li><a href="./giohang.php">Giỏ Hàng</a></li>
                 </ul>
             </div>
@@ -79,23 +52,8 @@ session_start();
 
 
 
+        <div class="spctmr">
 
-        <div class="spct mr">
-
-            <div class="boxa">
-                <div>
-                <img src="image/tải xuống.jpg" alt="">
-                </div>
-                <div>
-                    <img src="image/tải xuống.jpg" alt="">
-                </div>
-                <div>
-                    <img src="image/tải xuống.jpg" alt="">
-                </div>
-            </div>
-            <div class="boxct">
-
-            <h1> CHI TIẾT SẢN PHẨM</h1>
             <?php
 
             require "../models/connect.php";
@@ -108,29 +66,24 @@ session_start();
             ?>
 
 
+            <div class="anhsp">
+                <?php foreach ($product as $details) : ?>
 
-            <?php foreach ($product as $details): ?>
-
-                <img src="<?= " ../image/" . $details["image"] ?>" alt="">
-                <h3>
-                    <?= $details["descrtiption"] ?>
-                </h3>
-            <?php endforeach ?>
-            </div>
-            <div class="boxbl">
-                <?php foreach ($comment as $bl): ?>
-                    <div>
-                        <h4>
-                            <?= $bl["name"] ?>
-                        </h4>
-                        <p>
-                            <?= $bl["date"] ?>
-                        </p>
-                        <h4>
-                            <?= $bl["comment"] ?>
-                        </h4>
-                    </div>
+                    <img src="<?= " ../image/" . $details["image"] ?>" alt="">
                 <?php endforeach ?>
+            </div>
+            <div class="dathang">
+                <?php foreach ($product as $details) : ?>
+                    <h2>
+                        <?= $details["descrtiption"] ?>
+                    </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat, augue a volutpat hendrerit, sapien tortor faucibus augue, a maximus elit ex vitae libero. Sed quis mauris eget arcu facilisis consequat sed eu felis.
+                    </p>
+                <?php endforeach ?>
+
+
+
 
 
                 <?php
@@ -142,27 +95,26 @@ session_start();
                     $a = "SELECT * FROM products WHERE id = $id_p";
                     $p = getOne($a);
 
-                    ?>
+                ?>
                     <form action="../control/gioHang_control.php" method="POST" enctype="multipart/form-data">
                         <input type="text" hidden value="<?php echo $id_p ?>" name="id_p" id="">
                         <input type="text" hidden value="<?php echo $id_user ?>" name="id_user" id="">
-                        <input type="text" hidden value="<?php echo $p['namee'] ?>" name="name" id="">
+                        <input type="text" hidden value="<?php echo $p['name'] ?>" name="name" id="">
                         <input type="number" min="1" value="1" name="sl" id="">
                         <input type="text" hidden value="<?php echo $p['image'] ?>" name="image" id="">
                         <input type="text" hidden value="<?php echo $p['price'] ?>" name="price" id=""> <br>
+                        <b>Tình Trạng : Còn Hàng</b>
+                        <hr>
                         <a href="../control/gioHang_control.php"><button>Thêm vào giỏ hàng</button></a>
                     </form>
 
-                    <form action="../control/comment.php" method="POST">
-                        <div>
-                            <input type="text" hidden value="<?= $id ?>" name="id_p" id="">
-                            <input type="text" hidden value="<?= $_SESSION["email"] ?>" name="name" id="">
-                            <input type="text" hidden value="<?= $_SESSION["id"] ?>" name="id_u" id="">
-
-                            <textarea name="comment" id="" cols="30" rows="10" placeholder="sản phẩm tốt"></textarea>
-                            <button>Bình Luận</button>
-                        </div>
-                    </form>
+                    <div class="icon">
+                        <i class="fa-solid fa-heart"></i>
+                        <i class="fa-regular fa-envelope"></i>
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                    </div>
 
                 <?php
 
@@ -170,8 +122,66 @@ session_start();
                     echo " <button   ><a href='../viewND/dangNhap.php?'>đăng nhập để bình luận</a></button>";
                 }
                 ?>
-           
+            </div>
         </div>
+        <h3>Đánh giá sản phẩm</h3>
+        <div class="binhluansp">
+            <?php
+            if (isset($_SESSION["id"]) && ($_SESSION["id"]) > 0) {
+
+                $id_p = $_GET["id"];
+                $id_user = $_SESSION["id"];
+
+                $a = "SELECT * FROM products WHERE id = $id_p";
+                $p = getOne($a);
+
+            ?>
+                <form action="../control/comment.php" method="POST">
+                    <div>
+                        <input type="text" hidden value="<?= $id ?>" name="id_p" id="">
+                        <input type="text" hidden value="<?= $_SESSION["email"] ?>" name="name" id="">
+                        <input type="text" hidden value="<?= $_SESSION["id"] ?>" name="id_u" id="">
+                        <div class="chitietsp">
+                            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat, augue a volutpat hendrerit, sapien tortor faucibus augue, a maximus elit ex vitae libero. Sed quis mauris eget arcu facilisis consequat sed eu felis. Nunc sed porta augue. Morbi porta tempor odio, in molestie diam bibendum sed.</h4>
+                            <p>Weight: 0.3 kg
+                                Dimentions: 15 x 10 x 1 cm
+                                Colours: Black, Browns, White
+                                Material: Metal</p>
+
+                        </div>
+                        <textarea name="comment" id="topbinhluan" cols="180" rows="10" placeholder="sản phẩm tốt"></textarea>
+
+                    </div>
+                    <button>Bình Luận</button>
+                </form>
+
+            <?php
+            } else {
+                echo " <button   ><a href='../viewND/dangNhap.php?'>đăng nhập để bình luận</a></button>";
+            }
+            ?>
+        </div>
+
+
+
+        <div class="topbinhluan">
+            <?php foreach ($comment as $bl) : ?>
+                <div>
+                    <h3>
+                        <?= $bl["name"] ?>
+                    </h3>
+                    <h2>
+                        <?= $bl["date"] ?>
+                    </h2>
+                    <h4>
+                        <?= $bl["comment"] ?>
+                    </h4>
+                </div>
+            <?php endforeach ?>
+        </div>
+
+
+
     </div>
 
 </body>
