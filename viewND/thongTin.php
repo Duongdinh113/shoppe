11de02873594efce9f50300a49d395982d593ca5
thanh02 <122164005+thanhle02@users.noreply.github.com>
2023-04-11 00:05:57 +0700

    Update thongTin.php

--- viewND/thongTin.php
+++ viewND/thongTin.php
@@ -1,14 +1,51 @@
-<?php session_start()?>
-<form action="../control/control_add_bill.php" method="POST">
-    <div>
-        
-    <input hidden type="text" value="<?php echo $_SESSION['id']?>" name="id" id="">
-    <label for="">Số điện thoại</label>
-    <input type="text" name="sdt" id="">
-    <label for="">Họ và tên</label>
-    <input type="text" name="name" id="">
-    <label for="">địa chỉ</label>
-    <input type="text" name="diachi" id="">
+<?php session_start() ?>
+<style>
+    .thongtin {
+        max-width: 500px;
+        background-color: rgb(223, 242, 236);
+        height: 500px;
+        margin: 10% auto;
+
+    }
+    h1{
+        text-align: center;
+    }
+
+    input {
+        width: 400px;
+        height: 30px;
+    }
+
+    .tt {
+        margin-left: 10%;
+        padding-top: 50px;
+    }
+
+    button {
+        margin-top: 10%;
+        width: 200px;
+        height: 40px;
+        border-radius: 5px;
+        margin-left: 20%;
+        background: rgb(27, 27, 26);
+        color: white;
+        font-size: 25px;
+    }
+</style>
+<div class="thongtin">
+    <div class="tt">
+        <h1> THÔNG TIN </h1>
+        <form action="../control/control_add_bill.php" method="POST">
+            <div>
+                <input type="text" hidden value="<?php echo $_SESSION['id'] ?>" name="id" id="">
+                <p for="">Số điện thoại</p>
+                <input type="text" name="sdt" id="">
+                <p for="">Họ và tên</p>
+                <input type="text" name="name" id="">
+                <p for="">địa chỉ</p>
+                <input type="text" name="diachi" id="">
+            </div>
+            <button>mua</button>
+        </form>
     </div>
-    <button>mua</button>
-</form>
+</div>
