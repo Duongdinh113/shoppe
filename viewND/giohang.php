c5ef419a688fba6a342ff968883c47a394c18021
thanh02 <122164005+thanhle02@users.noreply.github.com>
2023-04-11 00:23:08 +0700

    Update giohang.php

--- viewND/giohang.php
+++ viewND/giohang.php
@@ -9,13 +9,21 @@ if (isset($_SESSION['id'] )&& ($_SESSION['id']>0)){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="css/giohang.css">
+   
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
    <link rel="stylesheet" href="css/layout.css">
     <title>GIỎ HÀNG </title>
 </head>
 <style>
+     .container {
+        max-width: 1200px;
+        padding: 0;
+        margin: 0 auto;
+        box-sizing: border-box;
+    }
 </style>
 <body>
+    <div class="container">
 <header>
             <div class="logo">
                 <img src="image/logo_1.1.png" alt="">
@@ -100,6 +108,7 @@ if (isset($_SESSION['id'] )&& ($_SESSION['id']>0)){
         </tbody>
     </table>
   <a href="./thongTin.php"><button>Thanh Toán</button></a>
+  </div>
 </body>
                 
 </html>
