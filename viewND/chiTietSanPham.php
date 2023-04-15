1c692dd05ca4b01031d382122f950e2ffed1bc80
Truongtraj <117931383+Truongtraj@users.noreply.github.com>
2023-04-16 02:44:51 +0700

    truong update new

--- viewND/chiTietSanPham.php
+++ viewND/chiTietSanPham.php
@@ -12,6 +12,106 @@ session_start();
     <link rel="stylesheet" href="./css/layout.css">
     <title>CHI TIẾT SẢN PHẨM </title>
 </head>
+<style>
+    .spctmr {
+        margin: 0 auto;
+        width: 1000px;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        column-gap: 60px;
+    }
+
+    .anhsp img {
+        width: 100%;
+    }
+
+    textarea {
+        width: 100%;
+    }
+
+
+
+    a.buynow {
+        padding: 10px 0px;
+        background-color: darkkhaki;
+        position: absolute;
+        bottom: -39px;
+        width: 100%;
+        transition: 0.25s ease-in-out;
+        text-align: center;
+        text-decoration: none;
+    }
+
+    .div a.anh img {
+        width: 100%;
+        display: block;
+        /*mat khoang trang ben duoi*/
+    }
+
+    .div a.anh {
+        display: block;
+    }
+
+    .div {
+        position: relative;
+        overflow: hidden;
+    }
+
+    .div:hover a.buynow {
+        bottom: 20px;
+    }
+
+    .sanpham {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        border-radius: 5px;
+        gap: 25px;
+        background-color: #F4F4F4;
+        padding: 15px 15px;
+    }
+
+    .sanpham1 {
+        /* width: 300px; */
+        /* border: 1px solid black; */
+        border-radius: 15px;
+        background-color: white;
+    }
+
+    .sanpham1 a img {
+        margin: 10px 10px;
+        width: 94%;
+        height: 300px;
+    }
+
+
+    .thoigian h4{
+        padding: 0px 0px 0px 90px;
+    }
+    .topbinhluan{
+        background-color: #f4f4f4;
+        border-radius: 10px;
+        padding: 15px 15px;
+    }
+    .topbinhluan h3{
+        background-color: white;
+        padding: 10px;
+    }
+    .khachhangbl{
+        background-color: white;
+        border-radius: 10px;
+        padding: 5px 5px;
+    }
+    .khachhangbl h3{
+        background-color: #FFFBF8;
+        border-radius: 10px;
+        padding-left: 50px;
+    }
+    .thoigian h5{
+        padding: 0px 0px 0px 90px;
+        color: #CCCCCC;
+    }
+
+</style>
 
 <body>
 
@@ -124,7 +224,7 @@ session_start();
                 ?>
             </div>
         </div>
-        <h3>Đánh giá sản phẩm</h3>
+        <h3>Thông tin sản phẩm</h3>
         <div class="binhluansp">
             <?php
             if (isset($_SESSION["id"]) && ($_SESSION["id"]) > 0) {
@@ -143,10 +243,13 @@ session_start();
                         <input type="text" hidden value="<?= $_SESSION["id"] ?>" name="id_u" id="">
                         <div class="chitietsp">
                             <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat, augue a volutpat hendrerit, sapien tortor faucibus augue, a maximus elit ex vitae libero. Sed quis mauris eget arcu facilisis consequat sed eu felis. Nunc sed porta augue. Morbi porta tempor odio, in molestie diam bibendum sed.</h4>
-                            <p>Weight: 0.3 kg
-                                Dimentions: 15 x 10 x 1 cm
-                                Colours: Black, Browns, White
-                                Material: Metal</p>
+                            <p>Weight: 0.3 kg</p>
+                            <p>Dimentions: 15 x 10 x 1 cm</p>
+                            <p>Colours: Black, Browns, White</p>
+                            <p>Material: Metal</p>
+
+
+
 
                         </div>
                         <textarea name="comment" id="topbinhluan" cols="180" rows="10" placeholder="sản phẩm tốt"></textarea>
@@ -165,23 +268,117 @@ session_start();
 
 
         <div class="topbinhluan">
+            <h3>Đánh giá sản phảm</h3>
             <?php foreach ($comment as $bl) : ?>
-                <div>
+                <div class="khachhangbl">
                     <h3>
                         <?= $bl["name"] ?>
                     </h3>
-                    <h2>
-                        <?= $bl["date"] ?>
-                    </h2>
-                    <h4>
-                        <?= $bl["comment"] ?>
-                    </h4>
+                    <div class="thoigian">
+                        <h4>
+                            -> <?= $bl["comment"] ?>
+                        </h4>
+                        <h5>
+                            <?= $bl["date"] ?>
+                        </h5>
+
+                    </div>
+
+                </div>
+            <?php endforeach ?>
+        </div>
+        <br>
+        <h3>Sản Phẩm Tương Tự</h3>
+        <?php if (empty($_POST["search"])) {
+            $sanpham = "SELECT * FROM products";
+        } else {
+            $search = $_POST["search"];
+
+            $sanpham = "SELECT * FROM products WHERE name LIKE '$search'";
+        }
+        $productList = getAll($sanpham) ?>
+        <div class="sanpham">
+            <?php foreach ($productList as $item1) : ?>
+                <div class="sanpham1">
+
+                    <a href="./chiTietSanPham.php?id=<?= $item1["id"] ?>"><img src="<?= "../image/" . $item1["image"] ?>" alt=""> </a>
+                    <h3>
+                        <?= $item1["name"] ?>
+                    </h3>
+                    <p>
+                        <?= $item1["descrtiption"] ?>
+                    </p>
+                    <span class="gia">
+                        <?= "$" .  $item1["price"]  ?>
+                    </span>
+
                 </div>
             <?php endforeach ?>
         </div>
 
 
 
+    </div>
+    <div class="page-footer">
+        <div class="footer">
+            <div class="footer-address">
+                <h2>HEROBIZ</h2>
+                <div class="address">
+                    <p>A108 Adam</p>
+                    <p>NY 535022</p>
+                </div>
+                <p>Phone:+1 5589</p>
+                <p>Emaillinfo@example.com</p>
+            </div>
+            <div class="footer-nav">
+                <div class="nav">
+                    <h3>Useful</h3>
+                    <p>Home</p>
+                    <p>About us</p>
+                    <p>Services</p>
+                    <p>Terms of</p>
+                    <p>Privacy</p>
+                </div>
+            </div>
+            <div class="nav1">
+                <h3>Our</h3>
+                <p>Web</p>
+                <p>Web</p>
+                <p>Product</p>
+                <p>Marketing</p>
+                <p>Graphic</p>
+            </div>
+            <div class="footer-contaect">
+                <div class="contaect">
+                    <h3>Our</h3>
+                    <p>Tamen quem nulla quae legam multos aute sint culpa legam moster magna</p>
+                </div>
+                <form action="">
+                    <input type="text" name="" id="">
+                    <button></button>
+                </form>
+            </div>
+        </div>
+        <div class="sub-footer">
+            <div class="chu">
+                <div class="end">
+                    <span>©</span>
+                    <span>HeroBiz</span>
+                    <span>. All Rights<span>
+                </div>
+                <div class="designed">
+                    <span>Designed</span>
+                    <span><a href="">BootstrapMade</a></span>
+                </div>
+            </div>
+            <div class="block">
+                <div class="block-same"></div>
+                <div class="block-same"></div>
+                <div class="block-same"></div>
+                <div class="block-same"></div>
+                <div class="block-same"></div>
+            </div>
+        </div>
     </div>
 
 </body>
